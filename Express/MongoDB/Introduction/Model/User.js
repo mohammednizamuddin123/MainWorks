4908c28f235f7f34f937224b204cb02f14c8308a
creator-works05 <creatorworks05@gmail.com>
2026-04-27 23:24:30 -0700

    280526

--- Express/MongoDB/Introduction/Model/User.js
+++ Express/MongoDB/Introduction/Model/User.js
@@ -1,7 +1,10 @@
 const mongoose=require("mongoose")
 const userSchema= new mongoose.Schema({
-    name:String,
-    email:String,
-    password:Number
+    name:{type:String,required:true,maxLength:15,minLength:3},
+    age:{type:Number,required:true,validate:{validator:v=>v>=18,message:props=>`must age 18 ${props.value}`}},
+    email:{type:String,required:true,unique:true,index:true,match:/.+\@.+\..+/},
+    password:{type:Number,required:true},
+    role:{type:String,default:"user"},
+    courses:{type:String,enum:["python","javascript","html","css"]}
 })
 module.exports=mongoose.model("users",userSchema)
